ae01723c0b8ecd7f90cd7af4b4db13402ad78355
HariomYadav18 <hariomchelsea@gmail.com>
2025-01-14 23:46:18 +0530

    dynamic counter of question nd score set up

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounterText');
-const scoreText = document.getElementById('scoreText');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
 
 let questions = [
     {
@@ -111,55 +110,57 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
-};
-
-getNewQuestion = () => {
+  };
+  
+  getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        //go to the end page
-        return window.location.assign("/end.html");
+      //go to the end page
+      return window.location.assign("/end.html");
     }
     questionCounter++;
     questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-
+  
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
-
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+  
+    choices.forEach(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
     });
-
+  
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
-};
-
-choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-
-        const classToApply = 
-         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        },1000);
-
-        });
-
-        
-        
-
+  };
+  
+  choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+      if (!acceptingAnswers) return;
+  
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
+  
+      const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+  
+      if (classToApply === "correct") {
+        incrementScore(CORRECT_BONUS);
+      }
+  
+      selectedChoice.parentElement.classList.add(classToApply);
+  
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 1000);
     });
-
-
-startGame();
+  });
+  
+  incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
+  
+  startGame();
 

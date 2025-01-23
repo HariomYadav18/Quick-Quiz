2cb5d4752928c6e6fca92032f6828bec52b35528
HariomYadav18 <hariomchelsea@gmail.com>
2025-01-24 00:54:45 +0530

    updated end page

--- end.js
+++ end.js
@@ -1,3 +1,14 @@
+const username = document.getElementById('username');
+const saveScoreButton = document.getElementById('saveScoreButton');
+
+
+username.addEventListener('keyup', () =>{
+    console.log(username.value);
+
+    saveScoreButton.Disabled = !username.value;
+})
+
+
 saveHighScore = (e) => {
     console.log("clicked the save button");
     e.preventDefault();

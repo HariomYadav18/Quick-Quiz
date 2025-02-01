fe5932861d2fe113f7338907ee92d7bcb6ba16b2
HariomYadav18 <hariomchelsea@gmail.com>
2025-02-01 14:10:44 +0530

    Recent score fixed

--- end.js
+++ end.js
@@ -1,6 +1,8 @@
-const username = document.getElementById('username');
+const username = document.getElementById("username");
 const saveScoreButton = document.getElementById('saveScoreButton');
-
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () =>{
     console.log(username.value);

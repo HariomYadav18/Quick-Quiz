d00484902619ff4d85968f639b05db5aba012c84
HariomYadav18 <hariomchelsea@gmail.com>
2025-06-14 02:11:20 +0530

    gitMerge branch 'main' of https://github.com/HariomYadav18/Quick-Quiz

--- end.js
+++ end.js
@@ -3,21 +3,17 @@ const saveScoreButton = document.getElementById('saveScoreButton');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-
 const MAX_HIGH_SCORES = 5;
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () =>{
+username.addEventListener('keyup', () => {
     saveScoreButton.disabled = !username.value;
 });
 
-
 saveHighScore = (e) => {
-    
     e.preventDefault();
 
     const score = {
@@ -27,12 +23,12 @@ saveHighScore = (e) => {
     
     highScores.push(score);
 
-    highScores.sort( (a,b) => b.score - a.score);
+    highScores.sort((a, b) => b.score - a.score);
 
     highScores.splice(5);
 
-localStorage.setItem('highScores', JSON.stringify(highScores));
-window.location.assign("/");
+    localStorage.setItem('highScores', JSON.stringify(highScores));
     
-    
+    // Fixed: Use relative path instead of absolute path
+    window.location.assign("index1.html");
 };
